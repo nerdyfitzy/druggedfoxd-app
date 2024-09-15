15d77fb49c0bbd792668922264942ceced9d2b50
nerdyfitzy <65741665+nerdyfitzy@users.noreply.github.com>
2024-09-15 12:48:41 -0400

    Update Lesson.tsx

--- components/Lesson.tsx
+++ components/Lesson.tsx
@@ -1,16 +1,71 @@
-import { View, Image, TouchableOpacity } from 'react-native';
+import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import React from 'react';
-import { Card, H3, Paragraph, XStack, YStack } from 'tamagui';
+import { Adapt, Card, H3, ListItem, Paragraph, Popover, XStack, YStack } from 'tamagui';
 import { Lesson as LessonType } from '~/constants/types';
 import { images } from '~/constants/images';
 import { CircleCheck, CircleX } from 'lucide-react-native';
 import * as Linking from 'expo-linking';
+import { Ellipsis, BookmarkPlus, Eye } from 'lucide-react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+  },
+  trigger: {
+    position: 'absolute',
+    right: 10,
+    top: 3,
+  },
+});
 
 const Lesson = (props: LessonType) => {
   return (
     <View>
       <TouchableOpacity onPress={() => Linking.openURL(props.link)}>
-        <Card padding="$2" width="$13" bordered={true} borderColor="$color5" marginBottom="$4">
+        <Card
+          style={styles.container}
+          padding="$2"
+          width="$13"
+          bordered={true}
+          borderColor="$color5"
+          marginBottom="$4">
+          <Popover allowFlip size="$6" placement="bottom-start">
+            <Popover.Trigger asChild>
+              <Ellipsis style={styles.trigger} />
+            </Popover.Trigger>
+
+            <Adapt>
+              <Popover.Sheet>
+                <Popover.Sheet.Frame>
+                  <Adapt.Contents />
+                </Popover.Sheet.Frame>
+                <Popover.Sheet.Overlay
+                  animation="lazy"
+                  enterStyle={{ opacity: 0 }}
+                  exitStyle={{ opacity: 0 }}
+                />
+              </Popover.Sheet>
+            </Adapt>
+            <Popover.Content
+              borderWidth={1}
+              borderColor="$borderColor"
+              enterStyle={{ y: -10, opacity: 0 }}
+              exitStyle={{ y: -10, opacity: 0 }}
+              elevate
+              animation={[
+                'quick',
+                {
+                  opacity: {
+                    overshootClamping: true,
+                  },
+                },
+              ]}>
+              <YStack>
+                <ListItem hoverTheme icon={BookmarkPlus} title="Bookmark Lesson" />
+                <ListItem hoverTheme icon={Eye} title="Mark as watched" />
+              </YStack>
+            </Popover.Content>
+          </Popover>
           <Card.Header>
             <H3>{props.player}</H3>
             <XStack gap="$2">
